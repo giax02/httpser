950439a4d7aa34c7df6587c0cfe04f89b2a052bd
Giacomol02 <giacomo.leandrini@hotmail.com>
2025-09-07 19:17:50 +0200

    Support for client disconnection, still broken tho

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #define MAX_CLIENT_COUNT 3
 #define SILENCE 1
 #define ACTIVE 1
+#define MAX_ATTEMPTS_ID_GEN 100
 
 // USE PORT 60100
 
@@ -19,38 +20,82 @@
 #include <fcntl.h>
 #include <malloc.h>
 
-struct client
-{
+struct client {
 	uint8_t id;		/*max 128 ids be careful*/
-	int position;	/*position in client_sockets array*/
-	struct pollfd;
+	unsigned int position;	/*position in client_sockets array*/
+	struct pollfd socket;
 	struct client * next_ptr;
 };
 
-void print_ver_and_id()
-{
+void print_ver_and_id() {
 	printf("Simple HTTP server\n");
-	printf("Version: ");
-	printf(VERSION);
-	printf("\n");
+	printf("Version: %s\n", VERSION);
 	printf("Author: Giacomo Leandrini\n");
 }
 
-void error(char *msg)
-{
+void error(char *msg) {
  perror(msg);
  exit(1);
 }
 
-void debug_printf(char buf[])
-{
+void debug_printf(char buf[]) {
 	if(!SILENCE) printf("%s", buf);
 }
 
-int main(int argc, char * argv[])
-{
+uint8_t id_gen(struct client * tail, struct client * head) {
+	if (tail == head) return rand();
+	for(int attempts = 1; i < MAX_ATTEMPTS_ID_GEN; attempts++) {
+		uint8_t id = rand();
+		while (tail->next_ptr != NULL) {
+			if (tail->id == id) break;
+			if (tail->next_ptr == NULL) return id;
+			tail = tail->nextptr;
+		}
+	}
+	return 0; //cant find a suitable id or max attempts reached
+}
 
-	struct client *head = NULL;
+/* (TAIL)<-()<-()<-()<-(HEAD) from main only tail is known, head can be obtained going through "next_ptr"s*/
+uint8_t add_client(struct client * tail, int sockfd) {
+	struct client * ttail = tail;
+	if(tail == NULL) {	/* first element */
+		tail = malloc(sizeof(struct client));
+		tail->next_ptr = NULL;
+		tail->id = id_gen(tail, tail);
+		tail->position = 1;
+		tail->socket.fd = sockfd;
+	} else {	/* every other element */
+		head = tail.next_ptr;
+		unsigned int ctr = 1
+		while (head->next_ptr != NULL) {
+			head = head->next_ptr;
+			ctr++;
+		}
+		head->next_ptr = malloc(sizeof(struct client));	/* head here is the latest added client */
+		head->next_ptr->next_ptr = NULL;
+		head->next_ptr->id = id_gen(tail, head);		/* to implement error check (id_gen can return 0) */
+		head->next_ptr->position = ctr + 1;						
+		head->next_ptr->socket.fd = sockfd;
+
+	}
+}
+
+void remove_client(unsigned int position, struct client * tail) {
+	struct client * ttail = tail;
+	while (ttail->next_ptr->position != (position - 1)) {
+		ttail = ttail->next_ptr;
+	}
+	struct client * temp = ttail->next_ptr;
+	free(temp);
+	ttail->next_ptr = ttail->next_ptr->next_ptr;
+}
+
+int main(int argc, char * argv[]) {
+
+	unsigned int seed = arc4random();
+	srand(seed);
+
+	struct client * tail = NULL;
 	struct client *clients = malloc(sizeof(struct client) * MAX_CLIENT_COUNT);
 	int sockfd, newsockfd, portno, clilen, n, client_count;
 	struct sockaddr_in serv_addr, cli_addr;
@@ -60,8 +105,7 @@ int main(int argc, char * argv[])
 
 	print_ver_and_id();
 
-	if (argc != 2)
-	{
+	if (argc != 2) {
 		fprintf(stderr, "Usage: %s port\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
@@ -98,13 +142,17 @@ int main(int argc, char * argv[])
 	debug_pr
 
 
-	while (1)
-	{
+	while (1) {
 		debug_printf("inizio while 1\n");
 		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-		if (newsockfd > 0)
-		{
-			//error("ERROR on accept");
+		if (newsockfd > 0) {
+			if(tail == NULL) {
+				tail->socket.fd = newsockfd;
+				tail->soc
+			}
+			else {
+
+			}
 			client_count++;
 			printf("Client %d connesso\n", client_count);
 			memset(&client_sockets[client_count], 0, sizeof(struct pollfd));
@@ -112,15 +160,12 @@ int main(int argc, char * argv[])
 			client_sockets[client_count].events = POLLIN;
 		}
 		//debug_printf("prima del if client count %d\n", client_count);
-		if(client_count >= 1)
-		{
+		if(client_count >= 1) {
 			//printf("if client_count maggiore di 1?");
 			int ret = poll(client_sockets, MAX_CLIENT_COUNT, 0);
-			for (int i = 1; i <= client_count; i++) //i is the client number
-			{
+			for (int i = 1; i <= client_count; i++) {
 				//printf("Itearazione %d",i);
-				if (client_sockets[i].revents & POLLIN)
-				{
+				if (client_sockets[i].revents & POLLIN) {
 					//memset(&buffer, 0, sizeof(buffer));
 					memset(&buffer, 0, sizeof(buffer));
 					n = read(client_sockets[i].fd, buffer, sizeof(buffer) - 1);
@@ -133,9 +178,3 @@ int main(int argc, char * argv[])
 	}
 	return 0;
 }
-
-	//fprintf(stdout, "%s", )
-		//bzero(buffer,256);
-		//n = read(newsockfd,buffer,255);
-		//if (n < 0) error("ERROR reading from socket");
-		//printf("Here is the message: %s",buffer);
